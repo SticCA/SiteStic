56a3650e644a1724565953422eca94713dabc71b
kperin <kristopher.perin@gmail.com>
2015-11-26 17:29:24 +0100

    add box

--- templates/index.php
+++ templates/index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="assets/css/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/flaticon.css">
     <link rel="stylesheet" href="assets/css/app_<?php echo $site; ?>.css">
+    <link rel="stylesheet" href="assets/css/app_global.css">
 </head>
 <body>
 
@@ -62,16 +63,38 @@
                 <h1 class="page-header">Master Méthodes Informatiques Appliquées à la Gestion des Entreprise - Amiens</h1>
 
                 <div class="content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis auctor leo, vel blandit elit. Phasellus id dolor eu purus scelerisque tristique eu ac mi. Proin eu sem enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis a semper erat. Phasellus ultricies velit quis arcu viverra, vitae bibendum lectus ultricies. Curabitur volutpat congue nisi ut aliquam. Duis luctus quis erat a sodales.</p>
-                    <p>Sed euismod purus quis ante mollis ornare. Etiam euismod purus quis dui iaculis pulvinar. Maecenas placerat nisl in nulla egestas facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis sagittis vehicula augue sed aliquet. Vivamus semper, sem vel vulputate dictum, lectus tellus ultrices purus, et posuere dolor elit non lorem. Fusce id eros leo. Proin lorem ex, facilisis in velit vitae, fermentum gravida augue.</p>
-                    <p>Suspendisse potenti. Morbi a vulputate lorem. Curabitur facilisis elit at enim ullamcorper viverra. Suspendisse euismod augue vitae diam commodo cursus. Nunc pretium, justo vitae commodo elementum, diam massa euismod urna, sit amet bibendum felis lorem id lectus. Nullam imperdiet lectus mauris, at sagittis nulla fermentum sed. Aliquam venenatis hendrerit metus, sed tincidunt lacus faucibus at. Aliquam gravida dapibus nisl sed volutpat. Nam sit amet congue mauris. Mauris pulvinar ante ac efficitur aliquam. Sed ut faucibus dolor. Sed vel aliquam tortor. Quisque id velit vel diam elementum elementum.</p>
-                    <p>Quisque ut consequat erat. Vivamus molestie euismod lectus, non rhoncus dui rutrum id. Cras consectetur varius arcu, quis porta tellus egestas non. Vivamus id sollicitudin diam, sed condimentum nisl. In tortor sapien, tristique nec risus id, commodo vehicula tortor. Proin dapibus est arcu. In tincidunt neque sed turpis dapibus mattis. Sed turpis elit, sollicitudin vel libero a, rutrum tincidunt dui.</p>
-                    <p>Integer sit amet eros luctus, consequat tortor at, pulvinar nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut scelerisque, lorem consectetur semper mollis, arcu magna venenatis dui, sit amet facilisis orci ipsum ac turpis. Sed consequat vel erat et consectetur. Nullam at orci dignissim, consectetur lacus ut, aliquet velit. Nulla convallis tellus libero, sit amet maximus leo sodales vel. Suspendisse nunc libero, lacinia id porttitor hendrerit, lobortis et quam. Sed sit amet augue vel velit sollicitudin posuere. Nullam elit ligula, vulputate et pretium facilisis, pulvinar placerat justo. Morbi vel quam feugiat, elementum augue molestie, aliquam dolor. Integer interdum vel felis ut fermentum.</p>
-                    <p>Integer dignissim velit quis placerat ultrices. Donec eget sapien condimentum nulla vestibulum viverra quis bibendum enim. Integer justo leo, tincidunt id odio tempus, bibendum aliquet lacus. Vestibulum et nulla eget nisl feugiat lacinia. Sed porttitor sem diam, quis feugiat ante pellentesque quis. Vivamus ut massa consequat, facilisis neque in, aliquet ligula. Integer ullamcorper, lorem vel venenatis lobortis, tortor odio congue nunc, eget ultrices enim purus a quam. Donec at purus convallis, posuere est vel, sagittis ex. Suspendisse odio arcu, volutpat et ipsum consequat, placerat aliquet orci. Donec risus lacus, scelerisque ut quam in, finibus rhoncus justo. Suspendisse eu ultrices neque. Nam bibendum sodales velit et ullamcorper. Curabitur in nulla quis massa porta condimentum sit amet a turpis. Morbi euismod magna quis convallis tempor. Quisque scelerisque enim in diam elementum, a feugiat purus rutrum.</p>
-                    <p>Suspendisse neque odio, mattis vel gravida at, sollicitudin ac nulla. Nullam a lacus non augue molestie vulputate. In mauris nulla, consequat tempor posuere eget, vulputate ut diam. Etiam scelerisque sagittis ante quis commodo. Quisque vitae libero sit amet augue posuere ultrices non vel urna. Sed aliquet tempor dolor porttitor rutrum. Phasellus elit justo, hendrerit in commodo nec, gravida eget mauris. Morbi gravida, nunc eu ullamcorper consectetur, sapien urna elementum elit, at rhoncus turpis diam et tortor. Proin eu mi a lectus suscipit volutpat. Nunc venenatis dolor congue tortor viverra, a aliquet ipsum ultrices. Vivamus mollis ex vel ex suscipit, at tristique lectus tempor. Sed euismod pellentesque dolor vel euismod. Donec nulla dui, finibus et ultricies non, aliquet at eros.</p>
-                    <p>Quisque sed accumsan neque, eu faucibus neque. Maecenas ac luctus purus. Duis sit amet nisi ac sapien finibus varius. Praesent non turpis turpis. Curabitur vel commodo ipsum, in feugiat elit. Ut vel maximus ipsum, at tincidunt mi. Pellentesque odio nunc, varius et facilisis vitae, feugiat nec metus. Cras lacinia tristique iaculis. Proin ac mi diam. Donec lacus ante, ullamcorper et est vitae, aliquet aliquam nibh. Nunc ut sagittis libero.</p>
-                    <p>Donec libero mauris, mattis faucibus malesuada nec, semper vitae lorem. Nunc augue nisi, egestas nec felis eget, luctus imperdiet elit. Etiam at interdum nunc. Nunc semper posuere mi, ac suscipit felis vestibulum id. Maecenas ac vestibulum purus. Morbi id orci vel nunc efficitur blandit. Aliquam nibh odio, placerat in varius ultrices, volutpat id lectus. Morbi sodales eros non aliquet vehicula. Sed rhoncus eu urna ac accumsan. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam eget est et nibh varius feugiat a nec erat. Nunc urna sapien, finibus id risus vitae, feugiat vehicula leo. In et massa in magna efficitur accumsan a at felis.</p>
-                    <p>Maecenas nibh justo, feugiat quis justo a, elementum luctus libero. Sed vestibulum semper facilisis. Duis tristique, dui vel hendrerit consequat, tellus libero pellentesque felis, quis vestibulum erat mauris id ligula. Vestibulum tempus quam eget arcu mollis, a rutrum neque imperdiet. Aenean porta, sapien in placerat gravida, urna neque rhoncus eros, quis sagittis risus dolor vel libero. Sed vitae interdum quam, sit amet rutrum purus. Curabitur ultrices dui velit, nec ultricies metus sodales quis. Proin sagittis, lacus non vestibulum tincidunt, tellus dolor porta quam, vel suscipit sem velit id tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer sit amet dui sed augue maximus aliquet sit amet non enim. Integer ultricies urna lacus, at vestibulum purus eleifend vitae.</p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="row box">
+                                <div class="col-md-4">
+                                    <div id="circle" style="border: 8px solid #1F9ADC;"><p>BAC +5</p></div>
+                                </div>
+                                <div class="col-md-8">
+                                    <p>Formation universitaire française de niveau Bac +5</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="row box">
+                                <div class="col-md-4">
+                                    <div id="circle" style="border: 8px solid #E8902C;"><p>x2</p></div>
+                                </div>
+                                <div class="col-md-8">
+                                    <p>Double compétence en informatique et en gestion</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="row box">
+                                <div class="col-md-4">
+                                    <div id="circle" style="border: 8px solid #4CC65E;"><p>20</p></div>
+                                </div>
+                                <div class="col-md-8">
+                                    <p>20 semaines de présence en entreprise par an</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </article>
         </div>
