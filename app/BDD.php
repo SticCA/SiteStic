3623c5cc0d625eddaec88eeae2ab633c30d59e00
unknown <kperin@AMI-F-PW07-4499.BETD.FR>
2015-12-04 15:39:59 +0100

    MAJ class BDD

--- app/BDD.php
+++ app/BDD.php
@@ -15,18 +15,41 @@ class BDD
     }
 
 
+    /**
+     * @param $table : nom de la table en BDD
+     * @param $data : tableau de valeur array(champ => valeur) ex : array("name"=>'toto',"pwd"=>123)
+     * @return bool
+     */
     public function InsertBDD($table, $data)
     {
-        $key = array_keys($data);
-        $champs = implode(",",$key);
-        $values = ":" . str_replace(",", ", :", $champs);
+        if(is_array($data) && (sizeof($data) > 0) && (!empty($table))) {
+            $key = array_keys($data);
+            $champs = implode(",", $key);
+            $values = ":" . str_replace(",", ", :", $champs);
 
-        $req = $this->bdd->prepare("INSERT INTO " . $table . "(".$champs.") VALUES (".$values.")");
+            $req = $this->bdd->prepare("INSERT INTO " . $table . "(" . $champs . ") VALUES (" . $values . ")");
 
-        if($req->execute($data)){
-            return true;
+            if ($req->execute($data)) {
+                return true;
+            } else {
+                return false;
+            }
         }else{
             return false;
+        }
+    }
+
+    /**
+     * @param $table
+     * @param $data
+     * @return null or value
+     */
+    public function SelectBDD($table, $data)
+    {
+        if(is_array($data) && (sizeof($data) > 0) && (!empty($table))) {
+            
+        }else{
+            return null;
         }
     }
 
